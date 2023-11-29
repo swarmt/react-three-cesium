d49564417d7032f92d355f3129940ff3b1a343c8
Sam Thomas <sam.thomas@dronedeploy.com>
2023-11-29 11:40:43 +1000

    Parameterise url, fix orientation

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
-import * as React from 'react'
-import { useEffect, useRef } from 'react'
-import * as ReactDOM from 'react-dom'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import './styles.css'
-import { TilesRenderer } from '3d-tiles-renderer'
-import { OrbitControls } from '@react-three/drei'
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
+import * as ReactDOM from 'react-dom';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import './styles.css';
+import { TilesRenderer } from '3d-tiles-renderer';
+import { OrbitControls } from '@react-three/drei';
 
-const Tiles = (): null => {
-  const { camera, gl, scene } = useThree()
-  const tilesRendererRef = useRef<TilesRenderer>()
+interface TilesProps {
+  url: string;
+}
+
+const Tiles: React.FC<TilesProps> = ({ url }) => {
+  const { camera, gl, scene } = useThree();
+  const tilesRendererRef = useRef<TilesRenderer>();
 
   useEffect(() => {
-    const tilesRenderer = new TilesRenderer('https://raw.githubusercontent.com/NASA-AMMOS/3DTilesSampleData/master/msl-dingo-gap/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize_tileset.json')
+    const tilesRenderer = new TilesRenderer(url);
     tilesRenderer.setCamera(camera);
     tilesRenderer.setResolutionFromRenderer(camera, gl);
     tilesRenderer.fetchOptions.mode = 'cors';
-    tilesRenderer.lruCache.minSize = 900;
-    tilesRenderer.lruCache.maxSize = 1300;
     tilesRenderer.errorTarget = 12;
+    tilesRenderer.group.rotation.set(Math.PI / 2, 0, 0);
 
     scene.add(tilesRenderer.group);
 
@@ -26,22 +29,22 @@ const Tiles = (): null => {
     return () => {
       scene.remove(tilesRenderer.group);
     };
-  })
+  }, [url, camera, gl, scene]);
 
   useFrame(() => {
-    if (!tilesRendererRef.current) return
+    if (!tilesRendererRef.current) return;
     const tilesRenderer = tilesRendererRef.current as TilesRenderer;
     camera.updateMatrixWorld();
     tilesRenderer.update();
-  })
+  });
 
-  return null
-}
+  return null;
+};
 
 ReactDOM.render(
   <Canvas>
-    <Tiles />
+    <Tiles url="https://raw.githubusercontent.com/NASA-AMMOS/3DTilesSampleData/master/msl-dingo-gap/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize_tileset.json" />
     <OrbitControls />
   </Canvas>,
   document.getElementById('root')
-)
+);
